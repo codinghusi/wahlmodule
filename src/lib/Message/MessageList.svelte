f3b9db390775902924aed9b95977094752f455a1
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2023-02-24 20:47:01 +0100

    hopefully fixed 'weird messages flickering at page load' no. 2

--- src/lib/Message/MessageList.svelte
+++ src/lib/Message/MessageList.svelte
@@ -2,8 +2,6 @@
 	import { messageStore } from './MessageStore';
 	import Message from './Message.svelte';
 
-	export const prerender = false;
-
 	let messages = [];
 
 	messageStore.subscribe(data => {
