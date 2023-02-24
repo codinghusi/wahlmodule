438fea3dcf9cc9beb0b27311d39fc6d9c42a92c5
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2023-02-24 22:24:35 +0100

    added spinner for page routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import Header from './Header.svelte';
 	import './styles.css';
 	import MessageList from '../lib/Message/MessageList.svelte';
+	import Spinner from '../lib/Data/Spinner.svelte';
+	import { navigating } from '$app/stores';
 </script>
 
 <div class="flex flex-col">
@@ -11,7 +13,9 @@
 
 	<div class="mt-4 p-1 flex justify-center">
 		<main class="max-w-xl w-full">
-			<slot />
+			<Spinner loading={$navigating} showContent={true}>
+				<slot />
+			</Spinner>
 		</main>
 	</div>
 
